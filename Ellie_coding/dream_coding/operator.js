7308bcb1ff3a0155a7bd851269fd141dd06da754
Hhhh Jjjj <didghwns0514@gmail.com>
2021-04-13 10:45:05 +0900

    2021.04.13-10:44

--- Ellie_coding/dream_coding/operator.js
+++ Ellie_coding/dream_coding/operator.js
@@ -53,3 +53,118 @@ console.log(`x : ${x}, y : ${y}`);
 //5. Comparison operators
 console.log(10 < 6);
 console.log(10 <= 6); // such....
+
+
+//6. Logical operators : || or,  && and,  ! not
+const value1 = false;
+const value2 = 4 < 2;
+// || or operator operation
+console.log('check OR operator : ', value1 || value2 || checker() );
+
+// CHECKER should be placed at the back
+function checker() {
+    for(let i=0; i<10; i++){
+        console.log('😎 waste some time!');
+    }
+    return true;
+}
+
+
+// 7. Equality
+const stringFive = '5';
+const new_stringFive = '5';
+const numberFive = 5;
+
+//== lose equality, with type conversion
+console.log(stringFive == numberFive); // true
+console.log(stringFive != numberFive); // false
+
+console.log(stringFive === numberFive); // false
+console.log(stringFive !== numberFive); // true
+
+console.log(stringFive === new_stringFive); // true
+console.log(stringFive == new_stringFive); // true
+
+
+// object
+const hj1 = {name: 'hj'};
+const hj2 = {name: 'hj'};
+const hj3 = hj1;
+console.log(hj1 == hj2); // false
+console.log(hj1 === hj2); // fasle
+console.log(hj1 === hj3); // true -> same reference pointers
+
+
+// equality quiz
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log('' == false); // true
+console.log('' === false); // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+//8. conditional operators : if
+// if, else if, else
+const name = 'coder';
+if(name === 'HoJun'){
+    console.log('Welcome, HoJun');
+}else if(name === 'coder'){
+    console.log('You are an amazing coder!');
+}else{
+    console.log('unknown!')
+}
+
+
+// 9. Ternary operator
+let name_ = 'HoJun'
+console.log(name_ === 'HoJun' ? 'yes' : 'no');
+
+/* 
+10. Switch Statement
+used for multiple if checks
+use for enum-like value check
+use for multiple type checks in TS
+*/
+const browser = 'IE';
+switch(browser){
+    case 'IE':
+        console.log('go away!');
+        break;
+    case 'Chrome':
+        console.log('love you!');
+        break;
+    case 'FireFox':
+        console.log('love you!');
+        break;
+    default:
+        console.log('same all!');
+        break;
+}
+
+// 11. loops
+let i = 3;
+while(i > 0){
+    console.log(`while: ${i}`);
+    i--;
+}
+
+
+// do while loop, body code is executed first,
+// then check the condition
+i = 3;
+do{
+    console.log(`do while: ${i}`);
+    i--;
+} while(i>0);
+
+
+// for loop, for(begin; condition; step)
+for(i =3; i >0; i--){
+    console.log(`for: ${i}`);
+}
+
+
+for(i=3; i > 0; i = i - 2){
+    // inline variable declaration
+    console.log(`inline variable for: ${i}`);
+}
